025f41ac899e0fb23fcab7d988e0c3dbbc3b29c2
123dipanjan <dipzsaha40@gmail.com>
2017-06-24 14:44:38 +0530

    Protected keyword example

---
+++ P2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package P2;
+
+import p1.P1;
+
+
+public class P2 extends P1 {
+    public static void main(String[] ar)
+    {
+        P2 obj=new P2();
+        obj.disp();
+                
+        
+    }
+}
